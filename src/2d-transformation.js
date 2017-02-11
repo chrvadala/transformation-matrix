a21b4eb7876a95964519494a433ceb614c6fcc4b
Christian Vadalà <chr.vadala@gmail.com>
2017-02-12 00:36:58 +0100

    adds methods skeleton

--- src/2d-transformation.js
+++ src/2d-transformation.js
@@ -8,3 +8,99 @@ export function identity() {
     d: 0, e: 1, f: 0
   }
 }
+
+/**
+ *
+ * @param matrices
+ */
+export function transform(...matrices) {
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param matrix
+ */
+export function inverse(matrix) {
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param tx
+ * @param ty
+ * @param matrix
+ */
+export function translate(tx, ty, matrix) {
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param sx
+ * @param sy
+ * @param matrix
+ */
+export function scale(sx, sy, matrix) {
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param angle
+ * @param matrix
+ */
+export function rotate(angle, matrix) {
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param angle
+ * @param matrix
+ */
+export function rotateDEG(angle, matrix){
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param point
+ * @param matrix
+ */
+export function applyToPoint(point, matrix) {
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param points
+ * @param matrix
+ */
+export function applyToPoints(points, matrix) {
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param matrix
+ */
+export function toCSS(matrix){
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param matrix
+ */
+export function toSVG(matrix){
+  throw new Error('not implemented yet')
+}
+
+/**
+ *
+ * @param matrix
+ */
+export function toString(matrix){
+  throw new Error('not implemented yet')
+}
