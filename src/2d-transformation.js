44840a5a2de5aa16da97eb08ab541644a3ae95f0
Christian Vadalà <chr.vadala@gmail.com>
2017-02-16 10:33:48 +0100

    adds inverse operation

--- src/2d-transformation.js
+++ src/2d-transformation.js
@@ -46,7 +46,22 @@ export function transform(...matrices) {
  * @param matrix
  */
 export function inverse(matrix) {
-  throw new Error('not implemented yet')
+  //http://www.wolframalpha.com/input/?i=Inverse+%5B%7B%7Ba,c,e%7D,%7Bb,d,f%7D,%7B0,0,1%7D%7D%5D
+
+  let {a, b, c, d, e, f} = matrix;
+
+  let denom = a * d - b * c;
+
+  return {
+    a: d / denom,
+    b: b / -denom,
+    c: c / -denom,
+    d: a / denom,
+    e: (d * e - c * f) / -denom,
+    f: (b * e - a * f) / denom
+  }
+
+
 }
 
 /**
--- tests/2d-transformation.spec.js
+++ tests/2d-transformation.spec.js
@@ -6,7 +6,8 @@ import {
   translate,
   scale,
   rotate,
-  rotateDEG
+  rotateDEG,
+  inverse
 } from '../src/2d-transformation';
 import chai from 'chai';
 
@@ -88,6 +89,31 @@ describe('2d-transformation', () => {
       });
       assert.deepEqual(applyToPoint({x: 0, y: 0}, m), {x: 40, y: 60});
       assert.deepEqual(applyToPoint({x: 50, y: 80}, m), {x: 90, y: 140});
+    })
+  });
+
+  describe('inverse', () => {
+    it('should return inverse matrix', () => {
+      // http://www.wolframalpha.com/input/?i=Inverse+%5B%7B%7B1,2,3%7D,%7B4,5,6%7D,%7B0,0,1%7D%7D%5D
+      const m = {
+        a: 1, c: 2, e: 3,
+        b: 4, d: 5, f: 6
+      };
+      const inv = {
+        a: -5 / 3, c: 2 / 3, e: 1,
+        b: 4 / 3, d: -1 / 3, f: -2
+      };
+      assert.deepEqual(inverse(m), inv)
+    });
+    it('should return original points', () => {
+      const m1 = { //trans(40,40) scale(2,2) trans(-40,-40)
+        a: 2, c: 0, e: -40,
+        b: 0, d: 2, f: -40
+      };
+      const points = [{x: 30, y: 30}, {x: 50, y: 50}];
+      const transPoints = [{x: 20, y: 20}, {x: 60, y: 60}];
+
+      assert.deepEqual(applyToPoints(transPoints, inverse(m1)), points);
     })
   });
 
