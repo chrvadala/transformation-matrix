8703ad77bf7c66f771e70655f2b7dba53aa961b2
Christian Vadalà <chr.vadala@gmail.com>
2017-02-23 12:04:23 +0100

    adds isAffineMatrix

--- src/2d-transformation.js
+++ src/2d-transformation.js
@@ -188,3 +188,25 @@ export function fromString(string) {
     f: parseFloat(parsed[6]),
   };
 }
+
+/**
+ * Check if the object contain an affine matrix
+ * @param object
+ * @return {boolean}
+ */
+export function isAffineMatrix(object) {
+  let isNumeric = n => typeof n === 'number' && !isNaN(n) && isFinite(n);
+  return typeof object === 'object'
+    && object.hasOwnProperty('a')
+    && isNumeric(object.a)
+    && object.hasOwnProperty('b')
+    && isNumeric(object.b)
+    && object.hasOwnProperty('c')
+    && isNumeric(object.c)
+    && object.hasOwnProperty('d')
+    && isNumeric(object.d)
+    && object.hasOwnProperty('e')
+    && isNumeric(object.e)
+    && object.hasOwnProperty('f')
+    && isNumeric(object.f);
+}
--- tests/2d-transformation.spec.js
+++ tests/2d-transformation.spec.js
@@ -8,7 +8,8 @@ import {
   rotate,
   rotateDEG,
   inverse,
-  fromString
+  fromString,
+  isAffineMatrix
 } from '../src/2d-transformation';
 import chai from 'chai';
 
@@ -206,6 +207,33 @@ describe('2d-transformation', () => {
       assert.throws(fromString.bind(this, 'matrix()'), /is not a matrix$/);
       assert.throws(fromString.bind(this, 'matrix(1,2,3,4,5)'), /is not a matrix$/);
       assert.throws(fromString.bind(this, 'matrix(a,b,c,d,e,f)'), /is not a matrix$/);
+    })
+  });
+
+  describe('isAffineMatrix', () => {
+    it('should return true', () => {
+      let o1 = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0};
+      let o2 = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, x: 100};
+      let o3 = {y: 200, a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, x: 100};
+      assert.isTrue(isAffineMatrix(o1));
+      assert.isTrue(isAffineMatrix(o2));
+      assert.isTrue(isAffineMatrix(o3));
+    });
+    it('should return false', () => {
+      let o1 = {a: 0, b: 0, c: 0, d: 0, e: 0};
+      let o2 = {a: 0, b: 0, c: 0, d: 0, e: 0, f: Number.NaN};
+      let o3 = {a: 0, b: 0, c: 0, d: 0, e: Number.POSITIVE_INFINITY, f: 0};
+      let o4 = {a: "0", b: 0, c: 0, d: 0, e: 0, f: 0};
+      let o5 = "{a: 0, b: 0, c: 0, d: 0, e: 0, f: 0}";
+      let o6 = {};
+      let o7 = 42;
+      assert.isFalse(isAffineMatrix(o1));
+      assert.isFalse(isAffineMatrix(o2));
+      assert.isFalse(isAffineMatrix(o3));
+      assert.isFalse(isAffineMatrix(o4));
+      assert.isFalse(isAffineMatrix(o5));
+      assert.isFalse(isAffineMatrix(o6));
+      assert.isFalse(isAffineMatrix(o7));
     })
   })
 });
