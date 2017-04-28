b8545a0fbd1d17c4385dc85d246f6744489b1203
Christian Vadalà <chr.vadala@gmail.com>
2017-04-28 13:36:37 +0200

    adds example

---
+++ misc/example.js
@@ -0,0 +1,10 @@
+const transformationMatrix = require("transformation-matrix");
+const {scale, rotate, translate, transform, applyToPoint} = transformationMatrix;
+
+let matrix = transform(
+  translate(40,40),
+  rotate(Math.PI/2),
+  scale(2, 4)
+);
+
+let point = applyToPoint(matrix, {x: 42, y: 42});
