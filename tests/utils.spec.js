db5bf462d67ee1c3fd3b531b7ca22cc5fdbc67c1
Christian Vadalà <chr.vadala@gmail.com>
2017-11-03 22:29:25 +0100

    defines isUndefined util

---
+++ tests/utils.spec.js
@@ -0,0 +1,13 @@
+import {assert} from 'chai'
+import {isUndefined} from "../src/utils";
+
+describe('isUndefined', () => {
+  it('should be undefined', () => {
+    assert.isTrue(isUndefined(undefined))
+
+    assert.isFalse(isUndefined(200))
+    assert.isFalse(isUndefined(Number(200)))
+    assert.isFalse(isUndefined(null))
+  })
+})
+
