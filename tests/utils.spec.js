cc2c29d0f883899ac166042023e02ab9e3dbfab4
Christian Vadalà <chr.vadala@gmail.com>
2017-11-03 23:09:59 +0100

    improves utils tests

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -1,13 +1,15 @@
 import {assert} from 'chai'
 import {isUndefined} from "../src/utils";
 
-describe('isUndefined', () => {
-  it('should be undefined', () => {
-    assert.isTrue(isUndefined(undefined))
+describe('utils', () => {
+  describe('isUndefined', () => {
+    it('should be undefined', () => {
+      assert.isTrue(isUndefined(undefined))
 
-    assert.isFalse(isUndefined(200))
-    assert.isFalse(isUndefined(Number(200)))
-    assert.isFalse(isUndefined(null))
+      assert.isFalse(isUndefined(200))
+      assert.isFalse(isUndefined(Number(200)))
+      assert.isFalse(isUndefined(null))
+    })
   })
 })
 
