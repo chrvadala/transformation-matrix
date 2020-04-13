1adcb68300642ae5597c13225d6edc77ce60188f
Christian <chr.vadala@gmail.com>
2020-04-13 20:07:54 +0200

    fixes scaling doc

--- src/scale.js
+++ src/scale.js
@@ -6,6 +6,8 @@ import { transform } from './transform'
  * Calculate a scaling matrix
  * @param sx {number} Scaling on axis x
  * @param [sy = sx] {number} Scaling on axis y (default sx)
+ * @param [cx] {number} If (cx,cy) are supplied the scaling is about this point
+ * @param [cy] {number} If (cx,cy) are supplied the scaling is about this point
  * @returns {Matrix} Affine Matrix
  */
 export function scale (sx, sy = undefined, cx = undefined, cy = undefined) {
