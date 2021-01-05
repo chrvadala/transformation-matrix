22192ef319bb5c1a02cd0a781daaba1e8f1b0a98
Christian <chr.vadala@gmail.com>
2021-01-05 20:00:03 +0100

    upgrades deps

--- src/applyToPoint.js
+++ src/applyToPoint.js
@@ -5,13 +5,15 @@
  * @returns {Point} Point
  */
 export function applyToPoint (matrix, point) {
-  return Array.isArray(point) ? [
-    matrix.a * point[0] + matrix.c * point[1] + matrix.e,
-    matrix.b * point[0] + matrix.d * point[1] + matrix.f
-  ] : {
-    x: matrix.a * point.x + matrix.c * point.y + matrix.e,
-    y: matrix.b * point.x + matrix.d * point.y + matrix.f
-  }
+  return Array.isArray(point)
+    ? [
+        matrix.a * point[0] + matrix.c * point[1] + matrix.e,
+        matrix.b * point[0] + matrix.d * point[1] + matrix.f
+      ]
+    : {
+        x: matrix.a * point.x + matrix.c * point.y + matrix.e,
+        y: matrix.b * point.x + matrix.d * point.y + matrix.f
+      }
 }
 
 /**
