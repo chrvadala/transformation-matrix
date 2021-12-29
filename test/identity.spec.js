babd0542aa8ccfcec51d0ed085645a83f6075836
Christian Vadalà <chr.vadala@gmail.com>
2021-12-30 00:47:56 +0100

    improves identity unit test

--- test/identity.spec.js
+++ test/identity.spec.js
@@ -1,5 +1,5 @@
 /* global describe, it, expect */
-import { identity } from '../src/index'
+import { identity } from '../src/identity'
 
 describe('identity', () => {
   it('should return identity matrix', () => {
